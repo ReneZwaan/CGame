0aba7b4f491b6393e4db0ec5c45604027a8ff9d9
Rene Zwaan <zwaan.r@gmail.com>
2016-01-02 19:28:39 +0800

    MadeProject
    Made the project inside of vs 2015

---
+++ cGame/Main.cpp
@@ -0,0 +1,7 @@
+#include <Windows.h>
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR     lpCmdLine, int       nCmdShow)
+{
+	unsigned int Test;
+	MessageBoxA(0, "Okey :(","Text",MB_OK|MB_ICONINFORMATION);
+}
